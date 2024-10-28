9d28077cb568247f09d6de9d4428b2b15b6d5382
JeanGargano <159099449+JeanGargano@users.noreply.github.com>
2024-10-28 18:14:58 -0500

    First Commit

---
+++ src/main/java/com/MOG_Backend/MogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.MOG_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MOG_Backend/MogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MOG_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
